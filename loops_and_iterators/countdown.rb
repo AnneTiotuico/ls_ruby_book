49905b4acc111ed5850d9790f8c537b74d5765ac
Anne Tiotuico <amtiotuico@gmail.com>
2020-08-16 04:16:55 +0000

    added until loop

--- loops_and_iterators/countdown.rb
+++ loops_and_iterators/countdown.rb
@@ -1,8 +1,22 @@
+
+# while loop: 
+=begin
 x = gets.chomp.to_i
 
 while x >= 0
   puts x
   x -= 1 # <- refactored this line
+end
+
+puts "Done!"
+=end
+
+# until loop that does the same as above: 
+x = gets.chomp.to_i
+
+until x < 0
+  puts x
+  x -= 1
 end
 
 puts "Done!"
