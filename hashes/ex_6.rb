6bfd8a7c8ad410ab578057fe812fe3881ccbc564
Anne Tiotuico <amtiotuico@gmail.com>
2020-08-17 22:57:42 +0000

    added ex_6.rb

---
+++ hashes/ex_6.rb
@@ -0,0 +1,7 @@
+# Given the following code...
+x = "hi there"
+my_hash = {x: "some value"}
+my_hash2 = {x => "some value"}
+
+# What's the difference between the two hashes that were created?
+# my_hash uses the symbol "x:", while my_hash2 uses the string value of "x" and is forced to use the => hash rocket syntax
