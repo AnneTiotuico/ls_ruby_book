a8c061f5991bfdbcb271182f32f13bed091504e9
Anne Tiotuico <amtiotuico@gmail.com>
2020-08-19 04:53:53 +0000

    added ex_3.rb

---
+++ more_stuff/ex_3.rb
@@ -0,0 +1,4 @@
+# What is exception handling and what problem does it solve?
+
+#exception handling is a process that deals with managing errors a program may encounter during execution; 
+#the problem it solves is that it lets your program output a message when it encounters and error, and continues as it normally would instead of breaking and exiting
